5fdf9de06b4d9bf172e69ceb3bae5c95a6c7e094
CarlosVasconcellos <44424001+CarlosVasconcellos@users.noreply.github.com>
2018-11-16 13:55:17 -0200

    Add files via upload

---
+++ Produto.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemadeestoques;
+
+/**
+ *
+ * @author labccet
+ */
+public class Produto {
+    public String nome;
+    public String fabricante;
+    
+    Produto() {
+        nome = "";
+        fabricante = "";
+    }
+    
+    Produto( String nm, String fabr ) {
+        nome = nm;
+        fabricante = fabr;
+    }
+}
